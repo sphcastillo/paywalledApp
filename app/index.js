e575a20d5f710a257dbc6c2ef32229ebdfeac12f
Sophia Castillo <sphcastillo@gmail.com>
2023-06-26 14:58:07 -0700

    added User folder & created Profile page. Connecting UserCard to profile page with expo-router

--- app/index.js
+++ app/index.js
@@ -1,73 +1,25 @@
-import { StyleSheet, Text, View, Image, ImageBackground} from "react-native";
-import users from "../assets/data/users";
+import { StyleSheet, View, FlatList } from "react-native";
+import  users from "../assets/data/users";
+import UserCard from "../src/components/UserCard";
 
-function UserCard({ user }) {
-
-  return (
-    <ImageBackground 
-    style={styles.userCard}
-    source={{ uri: user.coverImage }}
-  >
-      <View style={styles.overlay}/>
-      <Image 
-        src={user.avatar}
-        style={styles.userImage}
-      />
-      <View>
-        <Text 
-          style={{ 
-            color: "white", 
-            fontSize: 22, 
-            fontWeight: '500',
-            marginBottom: 5, 
-          }}
-          >{user.name}</Text>
-        <Text style={{ color: "white" }}>@{user.handle}</Text>
-      </View>
-    </ImageBackground>
-  )
-}
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <UserCard user={users[0]}/>
-      <UserCard user={users[1]} />
+
+      <FlatList 
+        data={users}
+        renderItem={({ item }) => <UserCard user={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
     padding: 10,
     paddingTop: 75,
   },
-  userCard: {
-    backgroundColor: "gray",
-    padding: 10,
-    flexDirection: "row",
-    alignItems: "flex-end",
-    borderRadius: 5,
-    // overflow: hidden needed for borderRadius to work 
-    overflow: "hidden",
-    marginVertical: 5, 
-  },
-  overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.4)",
-    // position: "absolute",
-    // top: 0,
-    // bottom: 0,
-    // left: 0,
-    // right: 0,
-    ...StyleSheet.absoluteFill,
-  },
-  userImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 100,
-    borderColor: "white",
-    borderWidth: 3,
-    marginRight: 20,
-  },
-});
+})
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,26 +1,66 @@
-function UserCard({ user }) {
+import { StyleSheet, View, Text, Image, ImageBackground, Pressable } from "react-native";
+import { Link } from 'expo-router';
 
-    return (
-      <ImageBackground 
-      style={styles.userCard}
-      source={{ uri: user.coverImage }}
-    >
-        <View style={styles.overlay}/>
-        <Image 
-          src={user.avatar}
-          style={styles.userImage}
-        />
-        <View>
-          <Text 
-            style={{ 
-              color: "white", 
-              fontSize: 22, 
-              fontWeight: '500',
-              marginBottom: 5, 
-            }}
-            >{user.name}</Text>
-          <Text style={{ color: "white" }}>@{user.handle}</Text>
-        </View>
-      </ImageBackground>
-    )
-  }
+export default function UserCard({ user }){
+
+  return (
+    <Link href={'/user'} asChild>
+      <Pressable>
+        <ImageBackground 
+        style={styles.userCard}
+        source={{ uri: user.coverImage }}
+        >
+          <View style={styles.overlay}/>
+          <Image   
+            src={user.avatar}
+            style={styles.userImage}
+          />
+          <View> 
+            <Text 
+              style={{ 
+                color: "white", 
+                fontSize: 22, 
+                fontWeight: '500',
+                marginBottom: 5, 
+              }}
+              >{user.name}</Text>
+            <Text >@{user.handle}</Text>
+          </View>
+        </ImageBackground>
+      </Pressable>
+    </Link>
+  )
+  
+}
+
+
+
+const styles = StyleSheet.create({
+  userCard: {
+    backgroundColor: "gray",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    borderRadius: 5,
+    // overflow: hidden needed for borderRadius to work 
+    overflow: "hidden",
+    marginVertical: 5, 
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    // position: "absolute",
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    ...StyleSheet.absoluteFill,
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    borderColor: "white",
+    borderWidth: 3,
+    marginRight: 20,
+  },
+})
