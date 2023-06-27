2081290d74eafdd6b05d7eb5f80ed1bced94065f
Sophia Castillo <sphcastillo@gmail.com>
2023-06-26 22:50:45 -0700

    Post.js / [id].js modified

--- app/user/[id].js
+++ app/user/[id].js
@@ -1,7 +1,11 @@
+import { View, FlatList, Text } from 'react-native';
 import { useRouter, useSearchParams } from 'expo-router';
 import users from "../../assets/data/users";
 import { useState } from 'react';
 import UserProfileHeader from '../../src/components/UserProfileHeader';
+import posts from "../../assets/data/posts";
+import Post from '../../src/components/Post';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const ProfilePage = () => {
     const [isSubscribed, setIsSubscribed] = useState(false);
@@ -15,12 +19,54 @@ const ProfilePage = () => {
         return <Text>User not found!</Text>
     }
 
+    if(!isSubscribed){
+        return (
+            <View>
+                <UserProfileHeader 
+                    user={user}
+                    isSubscribed={isSubscribed}
+                    setIsSubscribed={setIsSubscribed}
+                />
+                <View
+                    style={{ 
+                        backgroundColor: 'gainsboro',
+                        alignItems: 'center',
+                        padding: 20
+                    }}
+                >
+                    <FontAwesome5 name="lock" size={50} color="gray" />
+                    <Text
+                        style={{
+                            backgroundColor: 'royalblue',
+                            height: 50,
+                            borderRadius: 25,
+                            overflow: 'hidden',
+                            padding: 15,
+                            color: 'white',
+                            margin: 20
+                        }}
+                    >
+                        {' '}
+                        Subscribe to see user's posts
+                    </Text>
+                </View>
+                
+            </View>
+        )
+    }
+
     return (
-        <UserProfileHeader 
-            user={user}
-            isSubscribed={isSubscribed}
-            setIsSubscribed={setIsSubscribed}
-        />
+            <FlatList 
+                data={posts}
+                renderItem={({ item }) => <Post post={item} />}
+                ListHeaderComponent={() => (
+                    <UserProfileHeader 
+                        user={user}
+                        isSubscribed={isSubscribed}
+                        setIsSubscribed={setIsSubscribed}
+                />
+                )}
+            />
     )
 }
 
